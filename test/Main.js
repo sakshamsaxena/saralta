7eaef504d102c48cb5590cd36fe0c7a6c5db69a3
sakshamsaxena <ontenth@live.com>
2018-03-31 13:27:10 +0530

    Added all the tests I could think of. Onwards now!

--- test/Main.js
+++ test/Main.js
@@ -8,12 +8,90 @@
 
 describe('Forms API', function() {
 
-	it('should get Driving License Form');
+	describe('while getting forms', function() {
 
-	it('should get PAN Card Form');
+		it('should provide a form with Auth', function() {
+			// GET
+			// Auth : User | Admin
+		});
 
-	it('should get Voter ID Form');
+		it('should not provide a form without valid Auth', function() {
+			// GET
+			// Auth : Invalid
+		});
 
-	it('should get Birth Certificate Form');
+		it('should not provide a form without Auth', function() {
+			// GET
+			// Auth : None
+		});
+
+	});
+
+	describe('while submitting forms', function() {
+
+		it('should accept submission response with Auth', function() {
+			// POST
+			// Auth : User | Admin
+		});
+
+		it('should not accept submission response without valid Auth', function() {
+			// POST
+			// Auth : Invalid
+		});
+
+		it('should not accept submission response without Auth', function() {
+			// POST
+			// Auth : None
+		});
+
+	});
+	
+	describe('while creating forms', function() {
+
+		it('should create a new form with Admin Auth', function() {
+			// POST
+			// Auth : Admin
+		});
+
+		it('should not create a new form with User Auth', function() {
+			// POST
+			// Auth : User
+		});
+
+		it('should not create a new form without valid Admin Auth', function() {
+			// POST
+			// Auth : Invalid
+		});
+
+		it('should not create a new form without Auth', function() {
+			// POST
+			// Auth : None
+		});
+
+	});
+	
+	describe('while updating forms', function() {
+
+		it('should update an exiting form with Admin Auth', function() {
+			// PUT
+			// Auth : Admin
+		});
+
+		it('should not update an exiting form with User Auth', function() {
+			// PUT
+			// Auth : User
+		});
+
+		it('should not update an exiting form without valid Admin Auth', function() {
+			// PUT
+			// Auth : Invalid
+		});
+
+		it('should not update an exiting form without Auth', function() {
+			// PUT
+			// Auth : None
+		});
+
+	});
 
 });
